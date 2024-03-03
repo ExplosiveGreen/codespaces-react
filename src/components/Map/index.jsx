f9298eb7cf1207f4092a8006c6ffbb1ea434e8a5
Guy Rothstein <Guy.Rotshtein@stu.shenkar.ac.il>
2024-03-03 07:58:57 +0200

    Implemented routes service
    currently generating mock route, but code for actual route is already written and in comment (due to map getting all locations currently)

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,7 +1,7 @@
 import { GoogleMap, useJsApiLoader, Marker, InfoWindowF, DirectionsRenderer, DirectionsService} from '@react-google-maps/api';
 import { useDispatch,useSelector } from 'react-redux'
 import { addRoute,deleteRoute } from '../../../redux/actions/routes'
-import {useEffect, useState, useCallback, memo} from "react";
+import {useEffect, useState, useCallback, memo, useRef} from "react";
 
 const containerStyle = {
   width: '90%',
@@ -14,7 +14,8 @@ const center = {
   };
 
 function MyMap({locations, isDisplayRoute}) {
-
+    // let callCounter = 0
+    // console.log('test 0 - how many times am I called? it`s ', callCounter++)
     const [open, setOpen] = useState(false);
     
     const dispatch = useDispatch();
@@ -29,46 +30,24 @@ function MyMap({locations, isDisplayRoute}) {
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_API_KEY
     })
     useState
-
+    const waypts = [];
     const [map, setMap] = useState(null)
-    const [activeMarker, setActiveMarker] = useState(null);
-
-    const [response, setResponse] = useState<google.maps.DirectionsResult | null>(
-        null
-      )
-    // object for holding values of current route
-    const [directionsFormValue, setDirectionsFormValue] = useState({
-        origin: '',
-        destination: '',
-        travelMode: google.maps.TravelMode.DRIVING,
-    })
+    const [activeMarker, setActiveMarker] = useState(null)
+    const [origin, setOrigin] = useState(null)
+    const [destination, setDestination] = useState(null)
+    const [response, setResponse] = useState(null);
+    let count = useRef(0)
+    const [route, setRoute] = useState(null)
 
 
-    //2 objects holding references to current route's origin and destination
-    const originRef = useRef<HTMLInputElement | null>(null)
-    const destinationRef = useRef<HTMLInputElement | null>(null)
-
-    const directionsCallback = useCallback(
-        (
-          result,status
-        ) => {
-          if (result !== null) {
-            if (status === 'OK') {
-              setResponse(result)
-            } else {
-              console.log('response: ', result)
-            }
-          }
-        },
-        []
-      )
-
+      // this function runs once after loading
     const onLoad = useCallback(function callback(map) {
         
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
               (position) => {
                 setLatitude(position.coords.latitude);
+                setOrigin({lat: position.coords.latitude,lng: position.coords.longitude})
                 setLongitude(position.coords.longitude);
               },
               (error) => {
@@ -82,6 +61,16 @@ function MyMap({locations, isDisplayRoute}) {
         // const bounds = new window.google.maps.LatLngBounds(center);
         // map.fitBounds(bounds);
         setMap(map)
+        waypts.push({
+            location:{lat:32.151119, lng: 34.845105},
+            stopover: true,
+        })
+        waypts.push({
+            location:{lat:32.170437, lng:34.844282},
+            stopover: true,
+        })
+        setDestination({lat:32.504235,lng:35.001531})
+        
   }, [navigator.geolocation,locations])
 
     const onUnmount = useCallback(function callback(map) {
@@ -96,26 +85,25 @@ function MyMap({locations, isDisplayRoute}) {
         console.log(markerid);
     }
     
-    // sending to directions api your route destination, origin and travel mode.
-    const directionsServiceOptions =
-    useMemo<google.maps.DirectionsRequest>(() => {
-      return {
-        destination: directionsFormValue.destination,
-        origin: directionsFormValue.origin,
-        travelMode: directionsFormValue.travelMode,
-      }
-    }, [
-      directionsFormValue.origin,
-      directionsFormValue.destination,
-      directionsFormValue.travelMode,
-    ])
-
-    // getting response from directions api.
-    const directionsResult = useMemo(() => {
-        return {
-            directions: response,
+    const directionsCallback = (googleResponse) => {
+        if (googleResponse) {
+          if(response) {
+            if (googleResponse.status === 'OK' && googleResponse.routes.overview_polyline !== response.routes.overview_polyline) {
+              setResponse(() => googleResponse)
+              setRoute(googleResponse.routes[0])
+            } else {
+              console.log('response: ', googleResponse)
+            }
+          } else {
+            if (googleResponse.status === 'OK') {
+              setResponse(() => googleResponse)
+              setRoute(googleResponse.routes[0])
+            } else {
+              console.log('response: ', googleResponse)
+            }
+          }
         }
-    }, [response])
+      }
 
 
     return isLoaded ? (
@@ -128,97 +116,226 @@ function MyMap({locations, isDisplayRoute}) {
         onLoad={onLoad}
         onUnmount={onUnmount}
         >
+            {/* <>
+            {console.log('Returning map')}
+            </> */
+            isLoaded? console.log('loaded'):console.log('not loaded')
+            }
+            
         { /* Child components, such as markers, info windows, etc. */ }
+        {/* This is called TOO MANY TIMES!!! */}
+        {locations && isLoaded && locations.map((item, index)=>{
+            const lat = item.location.lat, lng=item.location.lng;
+            console.log('New iteration:')
+            return <>
+            {item.location &&<>
+                <Marker key={index} position={{ lat,lng }} onClick={() => handleActiveMarker(index)} onCloseClick={() => handleActiveMarker(none)}>
+                    {activeMarker === index ? (
+                        <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
+                                    {item.element}
+
+                        </InfoWindowF>
+                    ) : null}
+                </Marker>
+                </>}
+            </>
+        })}
         
-        {locations && locations.map((item, index)=>{
-                        const lat = item.location.lat, lng=item.location.lng;
-                        console.log('new marker:')
-                        console.log('lat: ',lat)
-                        console.log('lng: ',lng)
-                        return <>
-                        {item.location &&<>
-                            <Marker key={index} position={{ lat,lng }} onClick={() => handleActiveMarker(index)} onCloseClick={() => handleActiveMarker(none)}>
-                                {activeMarker === index ? (
-                                    <InfoWindowF onCloseClick={() => setActiveMarker(null)}>
-                                                {item.element}
-          
-                                    </InfoWindowF>
-                                ) : null}
-                            </Marker>
-                            </>}
-                        </>
-                    })}
-                    
-                    {isDisplayRoute && <Directions routesList={routes}/>}
+        {isDisplayRoute && <DirectionsMap origin={{lat, lng}} destination='' locations={locations}/>}
+        {isLoaded && <>
+            <div>
+          <>
+            {
+                waypts.push({
+                    location:{lat:32.151119, lng: 34.845105},
+                    stopover: true,
+                })
+            }
+            {
+                waypts.push({
+                    location:{lat:32.170437, lng:34.844282},
+                    stopover: true,
+                })
+            }
+            {destination !== '' && origin !== '' && (
+              <DirectionsService 
+                options={{
+                  origin,
+                  destination,
+                  waypoints: waypts,
+                  optimizeWaypoints: true,
+                  travelMode: 'DRIVING'
+                }}
+                callback={directionsCallback}
+              />
+            )}
+  
+            {response !== null && (
+              <DirectionsRenderer 
+                options={{
+                  directions: response
+                }}
+              />
+            )}
+          </>
+      </div>
+        </>}
         <></>
         </GoogleMap>
     ) : <></>
 }
-function Directions({routesList}){
-    const map = useMap();
-    const routesLibrary = useMapsLibrary("routes");
-    const [directionsService, setDirectionsService] = useState();
-    const [directionsRenderer, setDirectionsRenderer] = useState();
-    const [routes, setRoutes] = useState([]);
-    const [routeIndex, setRouteIndex] = useState(0);
-    const selected = routes[routeIndex];
-    const leg = selected?.legs[0]; // if we got more than 1 legs for the journey, which we DO, we can change leg to display the leg we're at!
 
-    useEffect(()=>{
-        if (!routesLibrary || !map) return;
-        setDirectionsService(new routesLibrary.DirectionsService());
-        setDirectionsRenderer(new routesLibrary.DirectionsRenderer({map}));
-    }, [routesLibrary, map]);
+function DirectionsMap(props) {
+    const waypts = [];
+    const {origin, destination,locations } = props;
+    const [mLocations,setLocations] = useState(props[2])
+    const [mOrigin, setOrigin] = useState(props[0])
+    const [mDestination, setDestination] = useState(props[1])
+    const [route, setRoute] = useState(null)
+    const [response, setResponse] = useState(null);
 
-    useEffect(()=>{
-        if (!directionsService || !directionsRenderer) return;
-        directionsService.route({
-            origin: "",
-            destination: "",
-            waypoints: routesList.map(item=>{
-                return {
-                    location: {
-                        lat: item[0], 
-                        lng: item[1]
-                    },
-                    stopover: true
-                }
-            }),
-            travelMode: google.maps.TravelMode.DRIVING,
-            provideRouteAlternatives: true
-        }).then(response => {
-            directionsRenderer.setDirections(response);
-            setRoutes(response.routes);
-        });
-    },[directionsService,directionsRenderer]);
+    //THIS FUNCTIONS SETS ALL LOCATIONS LOADED INTO THE ROUTE. DO NOT USE UNTIL YOU GET ONLY SPECIFIC ONES!!!!
+    
+    // locations.map((item, index)=>{
+    //     const lat = item.location.lat, lng=item.location.lng;
+    //     console.log('New stop in route!')
+    //     waypts.push({
+    //         location:{lat,lng},
+    //         stopOver: true
+    //     })
+    //     setDestination({lat,lng})
+    // })
+    //
+    //waypts.pop();
+    
+    // this is mock data:
 
-    useEffect(() => {
-        if (!directionsRenderer) return;
-        directionsRenderer.setRouteIndex(routeIndex);
-    }, [routeIndex, directionsRenderer]);
+    waypts.push({
+        location:{lat:32.151119, lng: 34.845105},
+        stopover: true,
+    })
+    waypts.push({
+        location:{lat:32.170437, lng:34.844282},
+        stopover: true,
+    })
 
-    if (!leg) return null;
+    setDestination({lat:32.504235,lng:35.001531})
+    // end of mock data
 
-    return <div className = "directions">
-        {/*Route name (generated):*/}
-        <h2>{selected.summary}</h2>
-        {/*route info (generated):*/}
-        <p>
-            {/*<START LOCATION> to <END LOCATION>*/}
-            {leg.start_address.split(",")[1]} to {leg.end_address.split(",")[1]}
-        </p>
-        <p>Distance: {leg.distance?.text}</p>
-        <p>Duration: {leg.duration?.text}</p>
+    const directionsCallback = (googleResponse) => {
+      if (googleResponse) {
+        if(response) {
+          if (googleResponse.status === 'OK' && googleResponse.routes.overview_polyline !== response.routes.overview_polyline) {
+            setResponse(() => googleResponse)
+            setRoute(googleResponse.routes[0])
+          } else {
+            console.log('response: ', googleResponse)
+          }
+        } else {
+          if (googleResponse.status === 'OK') {
+            setResponse(() => googleResponse)
+            setRoute(googleResponse.routes[0])
+          } else {
+            console.log('response: ', googleResponse)
+          }
+        }
+      }
+    }
+  
+    return (
+      <div>
+          <>
+            {destination !== '' && origin !== '' && (
+                
+              <DirectionsService 
+                options={{
+                  origin,
+                  mDestination,
+                  waypoints: waypts,
+                  optimizeWaypoints: true,
+                  travelMode: 'DRIVING'
+                }}
+                callback={directionsCallback}
+              />
+            )}
+  
+            {response !== null && (
+              <DirectionsRenderer 
+                options={{
+                  directions: response
+                }}
+              />
+            )}
+          </>
+      </div>
+    );
+  }
 
-    {/*    Displaying alternate routes here*/}
-        <h3>Other routes</h3>
-        <ul>
-            {routes.map((route, index) => <li key={route.summary}>
-                <button onClick={()=>setRouteIndex(index)}>
-                    {route.summary}
-                </button>
-            </li>)}
-        </ul>
-    </div>
-}
+// function Directions({routesList}){
+//     const map = useMap();
+//     const routesLibrary = useMapsLibrary("routes");
+//     const [directionsService, setDirectionsService] = useState();
+//     const [directionsRenderer, setDirectionsRenderer] = useState();
+//     const [routes, setRoutes] = useState([]);
+//     const [routeIndex, setRouteIndex] = useState(0);
+//     const selected = routes[routeIndex];
+//     const leg = selected?.legs[0]; // if we got more than 1 legs for the journey, which we DO, we can change leg to display the leg we're at!
+
+//     useEffect(()=>{
+//         if (!routesLibrary || !map) return;
+//         setDirectionsService(new routesLibrary.DirectionsService());
+//         setDirectionsRenderer(new routesLibrary.DirectionsRenderer({map}));
+//     }, [routesLibrary, map]);
+
+//     useEffect(()=>{
+//         if (!directionsService || !directionsRenderer) return;
+//         directionsService.route({
+//             origin: "",
+//             destination: "",
+//             waypoints: routesList.map(item=>{
+//                 return {
+//                     location: {
+//                         lat: item[0], 
+//                         lng: item[1]
+//                     },
+//                     stopover: true
+//                 }
+//             }),
+//             travelMode: google.maps.TravelMode.DRIVING,
+//             provideRouteAlternatives: true
+//         }).then(response => {
+//             directionsRenderer.setDirections(response);
+//             setRoutes(response.routes);
+//         });
+//     },[directionsService,directionsRenderer]);
+
+//     useEffect(() => {
+//         if (!directionsRenderer) return;
+//         directionsRenderer.setRouteIndex(routeIndex);
+//     }, [routeIndex, directionsRenderer]);
+
+//     if (!leg) return null;
+
+//     return <div className = "directions">
+//         {/*Route name (generated):*/}
+//         <h2>{selected.summary}</h2>
+//         {/*route info (generated):*/}
+//         <p>
+//             {/*<START LOCATION> to <END LOCATION>*/}
+//             {leg.start_address.split(",")[1]} to {leg.end_address.split(",")[1]}
+//         </p>
+//         <p>Distance: {leg.distance?.text}</p>
+//         <p>Duration: {leg.duration?.text}</p>
+
+//     {/*    Displaying alternate routes here*/}
+//         <h3>Other routes</h3>
+//         <ul>
+//             {routes.map((route, index) => <li key={route.summary}>
+//                 <button onClick={()=>setRouteIndex(index)}>
+//                     {route.summary}
+//                 </button>
+//             </li>)}
+//         </ul>
+//     </div>
+// }
 export default memo(MyMap)
